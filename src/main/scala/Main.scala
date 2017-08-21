03e9923877cff38368a7d74e540e689a1f5823ff
Matthew McGowan <MatthewMcGowan@users.noreply.github.com>
2017-08-21 20:51:36 +0100

    Dummy source printing to console.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,25 @@
 /**
   * Created by Matt on 21/08/2017.
   */
+import akka.stream._
+import akka.stream.scaladsl._
+
+import akka.{ NotUsed, Done }
+import akka.actor.ActorSystem
+import akka.util.ByteString
+import scala.concurrent._
+import scala.concurrent.duration._
+import java.nio.file.Paths
+
 object Main extends App {
+  implicit val system = ActorSystem("QuickStart")
+  implicit val materializer = ActorMaterializer()
+
+  val source: Source[Int, NotUsed] = Source(1 to 100)
+
+  val done: Future[Done] = source.runForeach(i => println(i))(materializer)
+
+  implicit val ec = system.dispatcher
+  done.onComplete(_ => system.terminate())
 
 }
