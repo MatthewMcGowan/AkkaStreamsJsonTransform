cbde0f555d5f231adf9b22bcc3ac0213d36592bf
Matthew McGowan <MatthewMcGowan@users.noreply.github.com>
2017-08-21 20:54:49 +0100

    Added processing, and file as sink

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,12 +14,16 @@ import java.nio.file.Paths
 object Main extends App {
   implicit val system = ActorSystem("QuickStart")
   implicit val materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
 
   val source: Source[Int, NotUsed] = Source(1 to 100)
 
-  val done: Future[Done] = source.runForeach(i => println(i))(materializer)
+  val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
 
-  implicit val ec = system.dispatcher
-  done.onComplete(_ => system.terminate())
+  val result: Future[IOResult] =
+    factorials
+      .map(num => ByteString(s"$num\n"))
+      .runWith(FileIO.toPath(Paths.get("factorials.txt")))
 
+  result.onComplete(_ => system.terminate())
 }
